41324a10a077db474a6e6537091ef4cbdf996251
seadonggyun4 <seadonggyun@gmail.com>
2022-01-08 14:48:41 +0900

    ✅ 타입스크립트 annotation

---
+++ typescript-basic/annotation.js
@@ -0,0 +1,9 @@
+"use strict";
+//변수를 annotation
+let a;
+a = 'hello'; // string 이외의 타입 x
+//매개변수를 annotation
+function hello(b) {
+    b = 'hello'; // string 이외의 타입 x
+    return b;
+}
---
+++ typescript-basic/annotation.ts
@@ -0,0 +1,11 @@
+//변수를 annotation
+let a:string 
+
+a = 'hello' // string 이외의 타입 x
+
+
+//매개변수를 annotation
+function hello(b:string){
+  b = 'hello' // string 이외의 타입 x
+  return b
+}
